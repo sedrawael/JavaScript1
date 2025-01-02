e19788abd9ada6d03f8aa758bbb8f7234fc5c279
sedrawael <gyrfhi123@gmail.com>
2025-01-02 03:47:52 +0300

    new

---
+++ tasks.js
@@ -0,0 +1,47 @@
+/*...1*/
+let x = 1000;
+let y = 500;
+let result = x / y;
+
+
+document.writeln(result);
+/*2*/
+let a = 1000;
+let b = 500;
+let result2 = a-b;
+document.writeln(result2);
+/*3*/
+let c = 1000;
+let d = 500;
+let result3 = c+d;
+document.writeln(result3);
+/*4*/
+let e = 1000;
+let f = 500;
+let result4 = f*e;
+document.writeln(result4);
+/*5*/
+let g = 7;
+let h = 9;
+let i = 2;
+let result5 = (g+h+i) /3 ;
+document.writeln(result5);
+
+/*6*/
+let j = 150; 
+let k =30 
+let result6 =(150 * 30)/100;
+let result7 =  150 - result6 ;
+document.writeln(result7)
+/*7*/
+let l = 18;
+let m =  30;
+let n =20 ;
+let result8 = (l <n< m);
+document.writeln(result8);
+/*8*/
+let number1 = 2;
+let number2 = 3;
+  
+let result9 = number1 ** number2 ;
+document.writeln(result9)
